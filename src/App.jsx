b65f23d95162d6648bad77590bebb150d27cc7d7
luany <luanyss@outlook.com>
2025-11-12 10:51:48 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+//aqui importamos os arquivos
+import Header from "./components/Header"
+
+//   header - tag
+// Header componente funcional -logica
+//arquivo principal
+//para escrita do nome do componente funcional  usamos sempre a primeira letra maiúscula, PascalCase
+function App() {
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
